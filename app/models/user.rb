ae30ab5720265935aab81193389274913e1fcc32
Justin Tung <JTung1104@gmail.com>
2016-02-23 10:12:34 -0800

    add user model, create users table, create users controller

---
+++ app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user if user && user.valid_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :profile)
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def generate_session_token
+    token = SecureRandom.urlsafe_base64(16)
+
+    while User.exists?(session_token: token)
+      token = SecureRandom.urlsafe_base64(16)
+    end
+
+    token
+  end
+end
