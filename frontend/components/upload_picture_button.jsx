9d6dbf487e84b00b5e46208d2216118469022fd2
Justin Tung <JTung1104@gmail.com>
2016-02-29 23:29:45 -0800

    remove nonfunctioning modal

--- frontend/components/upload_picture_button.jsx
+++ frontend/components/upload_picture_button.jsx
@@ -1,44 +1,8 @@
 var React = require('react');
-var Modal = require('react-modal');
-
-root = document.getElementById("root");
-Modal.setAppElement(root);
-
-var modalStyle = {
-  overlay : {
-    position          : 'fixed',
-    top               : 0,
-    left              : 0,
-    right             : 0,
-    bottom            : 0,
-    backgroundColor   : 'rgba(255, 255, 255, 0.75)',
-    background        : 'transparent'
-  },
-  content : {
-    position                   : 'absolute',
-    top                        : '40px',
-    left                       : '40px',
-    right                      : '40px',
-    bottom                     : '40px',
-    border                     : '1px solid #ccc',
-    background                 : '#fff',
-    overflow                   : 'auto',
-    WebkitOverflowScrolling    : 'touch',
-    borderRadius               : '4px',
-    outline                    : 'none',
-    padding                    : '20px'
-  }
-}
 
 var UploadPictureButton = React.createClass({
   getInitialState: function () {
     return { modalIsOpen: false };
-  },
-  openModal: function () {
-    this.setState({modalIsOpen: true});
-  },
-  closeModal: function () {
-    this.setState({modalIsOpen: false});
   },
   handleUploadPicture: function (e) {
     e.preventDefault();
@@ -60,17 +24,11 @@ var UploadPictureButton = React.createClass({
     return (
       <div>
         <a href="#"
-          className="upload-picture-button"
-          onClick={this.openModal}>
+          className="upload-picture-button">
           <img
             src="http://res.cloudinary.com/dsolojfgkabc/image/upload/instagram-photo-camera-logo-outline_xfplow.png"
             className="icon"/>
         </a>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closemodal}
-          style={modalStyle}>
-        </Modal>
       </div>
     );
   }
