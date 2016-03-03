659d766dfe99bd1d1767aeaee9f7883bad094ada
Justin Tung <JTung1104@gmail.com>
2016-03-03 11:24:33 -0800

    add auto scrolling when new comments are created

--- frontend/components/comment_table.jsx
+++ frontend/components/comment_table.jsx
@@ -1,10 +1,21 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var CommentItem = require('./comment_item');
 
 var CommentTable = React.createClass({
+  getInitialState: function () {
+    return {shouldScrollBottom: false};
+  },
+  componentWillUpdate: function() {
+    var node = ReactDOM.findDOMNode(this);
+    this.state.shouldScrollBottom = node.scrollTop + node.offsetHeight === (node.scrollHeight + 1);
+  },
   componentDidUpdate: function() {
-  var node = this.getDOMNode();
-  node.scrollTop = node.scrollHeight;
+    var comments = this.props.post.comments;
+    if (this.state.shouldScrollBottom || comments[comments.length - 1].user_id === currentUserId) {
+      var node = ReactDOM.findDOMNode(this);
+      node.scrollTop = (node.scrollHeight + 1)
+    }
   },
   render: function () {
     var that = this;
