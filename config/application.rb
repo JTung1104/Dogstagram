4c1eb6f4da8952aa94c82ccbcc490cbdfd7bd264
Justin Tung <JTung1104@gmail.com>
2016-03-31 12:51:13 -0700

    configure redis on heroku

--- config/application.rb
+++ config/application.rb
@@ -24,3 +24,17 @@ module Doggstagram
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+if ENV["REDISTOGO_URL"]
+  config = RedisDemoApp::Application.config
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+
+  config.cache_store = [
+    :redis_store, {
+      :host => uri.host,
+      :port => uri.port,
+      :password => uri.password,
+      :namespace => "cache"
+    }
+  ]
+end
