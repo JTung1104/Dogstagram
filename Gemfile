7aa4b254fa9537ab132fe216b49bccf08d80ffdd
Justin Tung <JTung1104@gmail.com>
2016-03-31 03:19:54 -0700

    change production settings add gzip compression gem

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
+gem 'heroku_rails_deflate', :group => :production
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,12 +28,14 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.serve_static_assets = true
+  config.assets.compress = true
+  config.assets.compile = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
