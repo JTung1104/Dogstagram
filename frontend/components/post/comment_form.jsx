03ad50f2b8fb3e74c01e9c5d2a719c9257ed8661
Justin Tung <JTung1104@gmail.com>
2016-11-16 20:01:33 -0800

    update comment_form to use es6

--- frontend/components/post/comment_form.jsx
+++ frontend/components/post/comment_form.jsx
@@ -1,39 +1,35 @@
-var React = require('react'),
-    ApiUtil = require('../../util/api_util');
+import React from 'react';
+import ApiUtil from '../../util/api_util';
 
-var CommentForm = React.createClass({
-  getInitialState: function () {
-    return { body: "" }
-  },
-  handleSubmit: function (e) {
-    e.preventDefault();
-    var that = this;
-
-    var comment = {
-      body: this.state.body,
-      post_id: this.props.post.id
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.setState({ body: "" });
+    }
+  
+    handleSubmit(event) {
+        event.preventDefault();
+        var comment = { body: this.state.body, post_id: this.props.post.id };
+        ApiUtil.createComment(comment, () => this.setState({body: ""}));
     }
 
-    ApiUtil.createComment(comment, function () {
-      that.setState({body: ""});
-    });
-  },
-  linkState: function (key) {
-    return (event => this.setState({[key]:event.currentTarget.value}));
-  },
-  render: function () {
-    return (
-      <div className="comment-form-div">
-        <form onSubmit={this.handleSubmit} className="comment-form">
-          <input className="comment-field"
-                 type="text"
-                 placeholder="Add a comment..."
-                 onChange={this.linkState('body')}
-                 value={this.state.body}/>
-        </form>
-      </div>
-    );
-  }
-});
+    linkState(key) {
+        return (event => this.setState({[key]:event.currentTarget.value}));
+    }
+    
+    render() {
+        return (
+            <div className="comment-form-div">
+                <form onSubmit={this.handleSubmit} className="comment-form">
+                    <input className="comment-field"
+                        type="text"
+                        placeholder="Add a comment..."
+                        onChange={this.linkState('body')}
+                        value={this.state.body}/>
+                </form>
+            </div>
+        );
+    }
+}
 
-module.exports = CommentForm;
+export default CommentForm;
