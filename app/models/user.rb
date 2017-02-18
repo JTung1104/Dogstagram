b47aa94429b2ac9bb1062bda546066fbb5b474ff
Justin Tung <JTung1104@gmail.com>
2017-02-17 16:13:17 -0800

    implement user model tests and add validation for :created_at

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
-  validates :password_digest, :session_token, presence: true
+  validates :password_digest, :session_token, :created_at, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   has_many :posts, dependent: :destroy
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+	subject { described_class.new }
+
 	it "is valid with valid attributes" do
-		expect(User.new).to_not be_valid 
+		subject.username = "jtung1104"
+		subject.password_digest = "digest"
+		subject.session_token = "tokentokentoken"
+		subject.created_at = DateTime.now
+		expect(subject).to be_valid 
 	end
 
-	it "is not valid without a username"
-	it "is not valid without a password digest"
-	it "is not valid without a session token"
-	it "is not valid without a created_at"
-	it "is not valid without a updated_at"
+	it "is not valid without a username" do
+		expect(subject).to_not be_valid
+	end
+
+	it "is not valid without a password digest" do
+		subject.username = "jtung1104"
+		expect(subject).to_not be_valid
+	end
+
+	it "is not valid without a session token" do 
+		subject.username = "jtung1104"
+		subject.password_digest = "digest"
+		subject.session_token = nil
+		expect(subject).to_not be_valid
+	end
+
+	it "is not valid without a created_at" do
+		subject.username = "jtung1104"
+		subject.password_digest = "digest"
+		subject.session_token = "tokentokentoken"
+		expect(subject).to_not be_valid
+	end 
+	
+	it "is valid without a updated_at" do
+		subject.username = "jtung1104"
+		subject.password_digest = "digest"
+		subject.session_token = "tokentokentoken"
+		subject.created_at = DateTime.now
+		expect(subject).to be_valid
+	end
 end 
