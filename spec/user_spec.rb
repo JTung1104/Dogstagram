ed923928498ef29fe052c2a4fb8c06df411a2b1a
Justin Tung <JTung1104@gmail.com>
2017-02-17 17:36:13 -0800

    add User model association tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -11,32 +11,44 @@ RSpec.describe User, :type => :model do
 		)
 	}
 
-	it "is valid with valid attributes" do
-		expect(subject).to be_valid 
-	end
+	describe "Validations" do
+		it "is valid with valid attributes" do
+			expect(subject).to be_valid 
+		end
 
-	it "is not valid without a username" do
-		subject.username = nil
-		expect(subject).to_not be_valid
-	end
+		it "is not valid without a username" do
+			subject.username = nil
+			expect(subject).to_not be_valid
+		end
 
-	it "is not valid without a password digest" do
-		subject.password_digest = nil
-		expect(subject).to_not be_valid
-	end
+		it "is not valid without a password digest" do
+			subject.password_digest = nil
+			expect(subject).to_not be_valid
+		end
 
-	it "is not valid without a session token" do 
-		subject.session_token = nil
-		expect(subject).to_not be_valid
-	end
+		it "is not valid without a session token" do 
+			subject.session_token = nil
+			expect(subject).to_not be_valid
+		end
 
-	it "is not valid without a created_at" do
-		subject.created_at = nil
-		expect(subject).to_not be_valid
-	end 
+		it "is not valid without a created_at" do
+			subject.created_at = nil
+			expect(subject).to_not be_valid
+		end 
+		
+		it "is valid without an updated_at" do
+			subject.updated_at = nil
+			expect(subject).to be_valid
+		end
+	end
 	
-	it "is valid without an updated_at" do
-		subject.updated_at = nil
-		expect(subject).to be_valid
+	describe "Associations" do 
+		it { should have_many(:posts) }
+		it { should have_many(:comments) }
+		it { should have_many(:likes) }
+		it { should have_many(:relationships) }
+		it { should have_many(:reverse_relationships) }
+		it { should have_many(:followers) }
+		it { should have_many(:followed_users) }
 	end
 end 
