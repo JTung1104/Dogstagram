70a92af3faa74599cbb70911cdd9a977b159025a
Justin Tung <JTung1104@gmail.com>
2017-02-17 17:09:41 -0800

    refactor user_spec.rb to dry up code

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,44 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-	subject { described_class.new }
+	subject { 
+		described_class.new(
+			username: "jtung1104",
+			password_digest: "digest", 
+			session_token: "tokentoken",
+			created_at: DateTime.now - 1.week,
+			updated_at: DateTime.now
+		)
+	}
 
 	it "is valid with valid attributes" do
-		subject.username = "jtung1104"
-		subject.password_digest = "digest"
-		subject.session_token = "tokentokentoken"
-		subject.created_at = DateTime.now
 		expect(subject).to be_valid 
 	end
 
 	it "is not valid without a username" do
+		subject.username = nil
 		expect(subject).to_not be_valid
 	end
 
 	it "is not valid without a password digest" do
-		subject.username = "jtung1104"
+		subject.password_digest = nil
 		expect(subject).to_not be_valid
 	end
 
 	it "is not valid without a session token" do 
-		subject.username = "jtung1104"
-		subject.password_digest = "digest"
 		subject.session_token = nil
 		expect(subject).to_not be_valid
 	end
 
 	it "is not valid without a created_at" do
-		subject.username = "jtung1104"
-		subject.password_digest = "digest"
-		subject.session_token = "tokentokentoken"
+		subject.created_at = nil
 		expect(subject).to_not be_valid
 	end 
 	
 	it "is valid without an updated_at" do
-		subject.username = "jtung1104"
-		subject.password_digest = "digest"
-		subject.session_token = "tokentokentoken"
-		subject.created_at = DateTime.now
+		subject.updated_at = nil
 		expect(subject).to be_valid
 	end
 end 
