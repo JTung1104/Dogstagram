395e1a31b68d504a5bfbaf34483a6c60c3c1edab
Justin Tung <JTung1104@gmail.com>
2017-02-18 18:25:42 -0800

    add post association tests

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,21 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-	subject { described_class.new }
+	subject { described_class.new(image_url: "https://www.randomurl.com/image.jpg", user_id: 52) }
 
-	it "is valid with valid attributes" do
-		subject.image_url = "https://www.randomurl.com/image.jpg"
-		subject.user_id = 1
-		expect(subject).to be_valid 
+	describe "Validations" do
+		it "is valid with valid attributes" do
+			expect(subject).to be_valid 
+		end
+
+		it "is not valid without an image url" do
+			subject.image_url = nil
+			expect(subject).to_not be_valid
+		end
+
+		it "is not valid without an user id" do
+			subject.user_id = nil
+			expect(subject).to_not be_valid
+		end
 	end
 
-	it "is not valid without an image url" do
-		subject.user_id = 1
-		expect(subject).to_not be_valid
-	end
-
-	it "is not valid without an user id" do
-		subject.image_url = "https://www.randomurl.com/image.jpg"
-		expect(subject).to_not be_valid
+	describe "Associations" do
+		it { should belong_to(:user) }
+		it { should have_many(:likes) }
+		it { should have_many(:comments) }
 	end
 end 
