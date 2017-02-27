4c45cc00fd707ce2ac1d985644c23012c41a9123
Justin Tung <JTung1104@gmail.com>
2017-02-26 16:31:25 -0800

    make profile_header css functional

--- frontend/components/profile/profile_header.jsx
+++ frontend/components/profile/profile_header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProfilePicture from './profile_picture'
 import ProfileStats from './profile_stats';
+import makeCn from '../../util/makeCn';
+const cn = makeCn({});
 
 var ProfileHeader = React.createClass({
   render: function () {
     return (
-      <header className="profile-header">
+      <header className={cn("-flex-center mv5")}>
         <ProfilePicture user={this.props.user}/>
         <ProfileStats user={this.props.user}/>
       </header>
